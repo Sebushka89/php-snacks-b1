23edfdc0db1b58631794d804b082d7813aae7183
Sebushka89 <sebastian.rizzo@hotmail.it>
2021-08-27 10:41:59 +0200

    ok

--- 5.php
+++ 5.php
@@ -5,8 +5,8 @@
 
 $text = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 
-$paragrafo = str_replace('.', '.<br>', $text);
+$paragrafo = str_replace('.', '.</p><p>', $text);
 
-echo $paragrafo;
+echo "<p>" . $paragrafo . "</p>";
 
 ?>
